a56c34eccf202602df171f2b5f98bc5d2a46558e
Aria <amini5454@gmail.com>
2022-01-09 12:07:35 -0600

    Vertically aligned linkedin photo for footer.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,16 +4,13 @@ import Image from "next/image";
 export default function Footer() {
     return (
         <footer className={styles.footer}>
-            <a
-                href="https://www.linkedin.com/in/aria-amini/"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                Powered by Spruce Technologies
-                <span className={styles.logo}>
-            <Image src="/linkedin.svg" alt="LinkedIn Logo" width={24} height={24}/>
-          </span>
-            </a>
+                <a href="https://www.linkedin.com/in/aria-amini/"
+                   target="_blank"
+                   rel="noopener noreferrer"
+                >
+                    <span className={styles.footerText}>Powered by Spruce Technologies</span>
+                    <Image className={styles.logo} src="/linkedin.svg" alt="LinkedIn Logo" width={24} height={24}/>
+                </a>
         </footer>
     );
 }
