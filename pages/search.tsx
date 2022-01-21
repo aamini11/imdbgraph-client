9efbf5db37cc8248c0d7e3cfa2f31f2e8846adb5
Aria <amini5454@gmail.com>
2022-01-21 01:04:19 -0600

    Fix click region not encompassing entire box

--- pages/search.tsx
+++ pages/search.tsx
@@ -16,7 +16,7 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
     }
 }
 
-export default function Search(props: {searchResults: Show[]}) {
+export default function Search(props: { searchResults: Show[] }) {
     return (
         <div className={styles.container}>
             <Head>
@@ -32,7 +32,7 @@ export default function Search(props: {searchResults: Show[]}) {
     )
 }
 
-function ShowList({shows}: {shows: Show[]}) {
+function ShowList({shows}: { shows: Show[] }) {
     return (
         <div className="divide-y divide-slate-100 divide-y-2">
             <Nav>
@@ -41,14 +41,14 @@ function ShowList({shows}: {shows: Show[]}) {
             </Nav>
             <List>
                 {shows.map((movie) => (
-                    <ListItem key={movie.imdbId} show={movie} />
+                    <ListItem key={movie.imdbId} show={movie}/>
                 ))}
             </List>
         </div>
     )
 }
 
-function Nav({children}: {children: ReactNode}) {
+function Nav({children}: { children: ReactNode }) {
     return (
         <nav className="py-4 px-6 text-sm font-medium">
             <ul className="flex space-x-3">
@@ -58,7 +58,7 @@ function Nav({children}: {children: ReactNode}) {
     )
 }
 
-function NavItem(props: {isActive: boolean, children: ReactNode}) {
+function NavItem(props: { isActive: boolean, children: ReactNode }) {
     return (
         <li>
             <a className={`block px-3 py-2 rounded-md ${props.isActive ? 'bg-sky-500 text-white' : 'bg-slate-50'}`}>
@@ -68,7 +68,7 @@ function NavItem(props: {isActive: boolean, children: ReactNode}) {
     )
 }
 
-function List({children} : {children: ReactNode}) {
+function List({children}: { children: ReactNode }) {
     return (
         <ul className="divide-y divide-slate-100">
             {children}
@@ -76,30 +76,33 @@ function List({children} : {children: ReactNode}) {
     )
 }
 
-function ListItem({show}: {show: Show}) {
+function ListItem({show}: { show: Show }) {
     return (
-        <article className="flex items-start space-x-6 p-6 drop-shadow-xl rounded-xl my-5 bg-gray-100 max-w-2xl">
-            <div className="min-w-0 relative flex-auto">
-                <h2 className="text-2xl font-semibold text-slate-900 truncate pr-20">{show.title}</h2>
-                <dl className="mt-2 flex flex-wrap text-sm leading-6 font-medium">
-                    <div className="absolute top-0 right-0 flex items-center space-x-1">
-                        <dt className="text-sky-500">
-                            <span className="sr-only">Star rating</span>
-                            <svg width="16" height="20" fill="currentColor">
-                                <path d="M7.05 3.691c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.372 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.539 1.118l-2.8-2.034a1 1 0 00-1.176 0l-2.8 2.034c-.783.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.363-1.118L.98 9.483c-.784-.57-.381-1.81.587-1.81H5.03a1 1 0 00.95-.69L7.05 3.69z" />
-                            </svg>
-                        </dt>
-                        <dd>{`${show.showRating.toFixed(1)} / 10.0`}</dd>
-                    </div>
-                    <div className="ml-2">
-                        <dt className="sr-only">Year</dt>
-                        <dd>{formatYears(show)}</dd>
-                    </div>
-                </dl>
-            </div>
+        <div className="relative max-w-2xl">
+            <article className="flex items-start space-x-6 p-6 drop-shadow-xl rounded-xl my-3 bg-gray-100">
+                <div className="min-w-0 relative flex-auto">
+                    <h2 className="text-2xl font-semibold text-slate-900 truncate pr-20">{show.title}</h2>
+                    <dl className="mt-2 flex flex-wrap text-sm leading-6 font-medium">
+                        <div className="absolute top-0 right-0 flex items-center space-x-1">
+                            <dt className="text-sky-500">
+                                <span className="sr-only">Star rating</span>
+                                <svg width="16" height="20" fill="currentColor">
+                                    <path
+                                        d="M7.05 3.691c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.372 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.539 1.118l-2.8-2.034a1 1 0 00-1.176 0l-2.8 2.034c-.783.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.363-1.118L.98 9.483c-.784-.57-.381-1.81.587-1.81H5.03a1 1 0 00.95-.69L7.05 3.69z"/>
+                                </svg>
+                            </dt>
+                            <dd>{`${show.showRating.toFixed(1)} / 10.0`}</dd>
+                        </div>
+                        <div className="ml-2">
+                            <dt className="sr-only">Year</dt>
+                            <dd>{formatYears(show)}</dd>
+                        </div>
+                    </dl>
+                </div>
+            </article>
             <Link href={`/ratings/${show.imdbId}`}>
-                <a className="absolute w-full h-full top-0 left-0 z-[1]"/>
+                <a className="absolute w-full h-full top-0 left-0 z-[1] m-0"/>
             </Link>
-        </article>
-    )
+        </div>
+    );
 }
