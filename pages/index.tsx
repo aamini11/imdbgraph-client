d895e5078e199ab5499ac18d35401eb4aa1f9ef9
Aria <amini5454@gmail.com>
2022-02-13 04:38:21 -0600

    Remove unused import

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,8 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import Searchbar from "../components/Searchbar";
 import Card from "../components/Card";
 import Footer from "../components/Footer";
+import Searchbar from "../components/Searchbar";
+import styles from '../styles/Home.module.css'
 
 export default function Home() {
     return (
