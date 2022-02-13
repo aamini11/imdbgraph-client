dfdc9890e50d201ab9a70f0fc9d4819174b83cec
Aria <amini5454@gmail.com>
2022-02-13 06:12:20 -0600

    Add home icon to search page

--- pages/search.tsx
+++ pages/search.tsx
@@ -20,20 +20,46 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
 export default function Search(props: { searchResults: Show[], searchTerm: string }) {
     return (
         <body>
-            <Head>
-                <title>IMDB Graph Search - {props.searchTerm}</title>
-                <meta name="description" content="Website to visualize IMDB TV show ratings as a graph"/>
-                <link rel="icon" href="/favicon.ico"/>
-            </Head>
+        <Head>
+            <title>IMDB Graph Search - {props.searchTerm}</title>
+            <meta name="description" content="Website to visualize IMDB TV show ratings as a graph"/>
+            <link rel="icon" href="/favicon.ico"/>
+        </Head>
 
-            <main className="max-w-screen-sm p-6">
-                <Searchbar/>
-                {props.searchResults.length > 0
-                    ? <List searchResults={props.searchResults}/>
-                    : `No results found for : ${props.searchTerm}`
-                }
-            </main>
+        <main className="max-w-screen-sm p-6">
+            <Navigation/>
+            {props.searchResults.length > 0
+                ? <List searchResults={props.searchResults}/>
+                : `No results found for : ${props.searchTerm}`
+            }
+        </main>
         </body>
+    );
+}
+
+function Navigation() {
+    return (
+        <div className="flex items-center">
+            <HomeIcon/>
+            <Searchbar/>
+        </div>
+    );
+}
+
+function HomeIcon() {
+    return (
+        <Link href="/" passHref>
+            <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+                 width="36" height="36"
+                 viewBox="0 0 24 24"
+                 className="m-2 cursor-pointer transition hover:fill-blue-500"
+            >
+                <a>
+                    <path
+                        d="M 12 2 A 1 1 0 0 0 11.289062 2.296875 L 1.203125 11.097656 A 0.5 0.5 0 0 0 1 11.5 A 0.5 0.5 0 0 0 1.5 12 L 4 12 L 4 20 C 4 20.552 4.448 21 5 21 L 9 21 C 9.552 21 10 20.552 10 20 L 10 14 L 14 14 L 14 20 C 14 20.552 14.448 21 15 21 L 19 21 C 19.552 21 20 20.552 20 20 L 20 12 L 22.5 12 A 0.5 0.5 0 0 0 23 11.5 A 0.5 0.5 0 0 0 22.796875 11.097656 L 12.716797 2.3027344 A 1 1 0 0 0 12.710938 2.296875 A 1 1 0 0 0 12 2 z"/>
+                </a>
+            </svg>
+        </Link>
     );
 }
 
