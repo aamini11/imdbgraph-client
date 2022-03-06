d9fda63f66016514c1acb8b3dd02f513e2ade6d2
Aria <amini5454@gmail.com>
2022-03-06 03:01:10 -0600

    Add Unit testing library (Jest) + Sample

---
+++ __tests__/Home.test.tsx
@@ -0,0 +1,36 @@
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import Home from "../pages";
+
+describe("Home page tests", () => {
+    it("Renders title correctly", () => {
+        render(<Home />);
+
+        const mainTitle = screen.getByRole("heading", {
+            name: "Welcome to IMDB Graph",
+        });
+
+        expect(mainTitle).toBeInTheDocument();
+    });
+
+    it("Renders Gitlab page correctly", () => {
+        render(<Home />);
+
+        const title = "Source Code →";
+        expect(
+            screen.getByRole("heading", {
+                name: title,
+                level: 2,
+                exact: false,
+            })
+        ).toBeInTheDocument();
+
+        const description = "IMDB Graph is 100% open source. Click here to see the GitLab page";
+        expect(screen.getByText(description)).toBeInTheDocument();
+
+        const link = screen.getByRole("link", {
+            name: title + " " + description,
+        });
+        expect(link).toHaveAttribute("href", "https://gitlab.com/users/aamini11/projects");
+    });
+});
