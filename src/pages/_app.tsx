23e26c567f12de770e8b7ac936c89357077ae9f6
Aria <amini5454@gmail.com>
2022-11-06 01:57:02 -0500

    Use Inter font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
+import { Inter } from "@next/font/google";
+
 import type { AppProps } from "next/app";
 import "../../styles/globals.css";
 
+// If loading a variable font, you don't need to specify the font weight
+// https://nextjs.org/docs/basic-features/font-optimization
+const inter = Inter({ subsets: ['latin'] });
+
 export default function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+    return (
+      <main className={inter.className}>
+          <Component {...pageProps} />
+      </main>
+    )
 }
