9439af5086987487c0f459420edb28e0270a5bc3
Aria <amini5454@gmail.com>
2022-11-19 00:54:58 -0600

    Fix cut off text in ratings page

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,3 +1,3 @@
 export default function Title(props: { text: string }) {
-    return <h1 className="text-center m-0, text-6xl">{props.text}</h1>;
+    return <h1 className="text-center text-6xl leading-tight">{props.text}</h1>;
 }
