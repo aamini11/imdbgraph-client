72b82588585a98b3eb6fea5aa3dc1f6c2c7c58dc
Aria <amini5454@gmail.com>
2023-07-30 10:40:49 -0400

    Minor clean up for config files

--- jest.setup.js
+++ jest.setup.js
@@ -1,10 +1,9 @@
 // Optional: configure or set up a testing framework before each test.
 // If you delete this file, remove `setupFilesAfterEnv` from `jest.config.js`
-
-// Used for __tests__/testing-library.js
-// Learn more: https://github.com/testing-library/jest-dom
+import "@testing-library/jest-dom";
 import "@testing-library/jest-dom/extend-expect";
 import { TextDecoder, TextEncoder } from "util";
 
+// https://github.com/inrupt/solid-client-authn-js/issues/1676#issuecomment-917016646
 global.TextEncoder = TextEncoder;
 global.TextDecoder = TextDecoder;
--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
-/**
- * @type {import('next').NextConfig}
- */
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
     swcMinify: true,
     reactStrictMode: true,
     i18n: {
@@ -18,3 +16,5 @@ module.exports = {
         ];
     },
 };
+
+module.exports = nextConfig;
