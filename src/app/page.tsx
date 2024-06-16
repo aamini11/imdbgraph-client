4cff1cca64e9e526f4d6676ec0eaa33d671a499e
Aria <amini5454@gmail.com>
2024-06-16 01:20:50 -0400

    Minor card style changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -60,9 +60,9 @@ function Card(props: { title: string; body: string }) {
             <a
                 href="https://gitlab.com/users/aamini11/projects"
                 className="max-w-[300px] p-6 rounded-[10px]
-                           dark:bg-neutral-900
-                           border-2 border-[#eaeaea]
-                           transition hover:text-blue-600 hover:border-blue-600 hover:ease-in duration-200"
+                           dark:bg-neutral-950
+                           border-2 border-neutral-700
+                           transition hover:text-blue-300 hover:border-blue-300 hover:ease-in duration-200"
             >
                 <h2 className="mt-0 mr-0 mb-4 ml-0 text-2xl whitespace-nowrap">{props.title}</h2>
                 <p className="m-0 text-lg">{props.body}</p>
