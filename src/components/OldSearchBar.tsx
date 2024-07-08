6e1635d8cf24325e87d2dd3b6a3baba94fb55742
Aria <amini5454@gmail.com>
2024-07-08 04:35:37 -0400

    WIP

--- src/components/OldSearchBar.tsx
+++ src/components/OldSearchBar.tsx
@@ -2,10 +2,10 @@
 
 import { Input, ScrollShadow, Spinner } from "@nextui-org/react";
 import { clsx } from "@nextui-org/shared-utils";
+import { useCombobox, UseComboboxReturnValue } from "downshift";
 import debounce from "lodash/debounce";
-import Link from "next/link";
 import { useRouter } from "next/navigation";
-import { useEffect, useMemo, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import { SearchIcon } from "@/components/Icons";
 import { formatYears, Show } from "@/models/Show";
 
@@ -16,57 +16,30 @@ export function Searchbar() {
     const router = useRouter();
 
     const [text, setText] = useState("");
-    const { suggestions, isLoading } = useSuggestions(text);
-    const [isFocused, setIsFocused] = useState(false);
-    const [selected, setSelected] = useState<number>();
-    const selectedShow = selected !== undefined ? suggestions[selected] : undefined;
     const [isRedirecting, setIsRedirecting] = useState(false);
 
-    const isDropdownVisible = isFocused && text.length > 0 && suggestions.length > 0;
+    const { suggestions, isLoading } = useSuggestions(text);
 
-    function navigateDropdown(direction: "ArrowUp" | "ArrowDown") {
-        if (!isDropdownVisible) {
-            return;
-        }
+    const comboBoxProps = useCombobox({
+        items: suggestions,
+        onInputValueChange: ({ inputValue }) => setText(inputValue),
+        onSelectedItemChange: ({ selectedItem }) => onFormSubmit(selectedItem),
+        itemToString: (show) => show?.title ?? "",
+    });
+    const { isOpen, getInputProps, getMenuProps } = comboBoxProps;
 
-        const dropdownSize = suggestions.length;
-
-        let newSelected;
-        if (selected == null) {
-            newSelected = direction == "ArrowDown" ? 0 : dropdownSize - 1;
-            setSelected(newSelected);
-        } else if (
-            (selected === 0 && direction === "ArrowUp") ||
-            (selected === dropdownSize - 1 && direction === "ArrowDown")
-        ) {
-            newSelected = undefined;
-            setSelected(newSelected);
-        } else {
-            newSelected = direction === "ArrowDown" ? selected + 1 : selected - 1;
-            setSelected(newSelected);
-        }
-    }
-
-    const onUserTyping = (input: string) => {
-        setSelected(undefined);
-        setText(input);
-    };
-
-    const onFormSubmit = () => {
+    const onFormSubmit = (show: Show) => {
         if (isEmpty(text)) {
             return;
         }
 
-        setIsRedirecting(true);
-        if (document.activeElement instanceof HTMLElement) {
-            document.activeElement.blur();
-        }
-        if (selectedShow === undefined) {
-            // Go to search page
-            router.push(`/search?q=${text}`);
-        } else {
+        if (show) {
+            setIsRedirecting(true);
+            if (document.activeElement instanceof HTMLElement) {
+                document.activeElement.blur();
+            }
             // Go to ratings page
-            goToRatingsPage(selectedShow.imdbId);
+            goToRatingsPage(show.imdbId);
         }
     };
 
@@ -75,37 +48,12 @@ export function Searchbar() {
     };
 
     return (
-        <form
-            role="search"
-            className="relative w-full"
-            onSubmit={(e) => {
-                onFormSubmit();
-                e.preventDefault();
-            }}
-        >
+        <div className="relative w-full">
             <Input
-                autoComplete="off"
                 variant="bordered"
                 radius="full"
-                id="search-bar"
                 type="text"
-                role="combobox"
-                aria-label="TV Show"
-                aria-owns="tv-search-dropdown"
-                aria-autocomplete="list"
-                aria-expanded={isDropdownVisible}
-                aria-activedescendant={selectedShow?.imdbId ?? ""}
                 placeholder="Search for any TV show..."
-                value={!selectedShow ? text : selectedShow.title}
-                onChange={(e) => onUserTyping(e.currentTarget.value)}
-                onFocus={() => setIsFocused(true)}
-                onBlur={() => setIsFocused(false)}
-                onKeyDown={(e) => {
-                    if (e.key == "ArrowUp" || e.key == "ArrowDown") {
-                        e.preventDefault();
-                        navigateDropdown(e.key);
-                    }
-                }}
                 classNames={{
                     input: "ml-1",
                     inputWrapper: "h-[48px]",
@@ -113,57 +61,60 @@ export function Searchbar() {
                 startContent={<SearchIcon className="text-default-400" strokeWidth={2.5} size={20} />}
                 endContent={(isLoading || isRedirecting) && <Spinner color="default" size="sm" />}
                 disabled={isRedirecting}
+                {...getInputProps()}
             />
-            <div id="search-suggestions">
-                {isDropdownVisible && <DropDown suggestions={suggestions} selectedShow={selectedShow} />}
+            <div {...getMenuProps()} className="absolute w-full z-10 overflow-clip">
+                {isOpen && suggestions.length > 0 && (
+                    <DropDown suggestions={suggestions} comboBoxProps={comboBoxProps} />
+                )}
             </div>
-        </form>
+        </div>
     );
 }
 
-function DropDown(props: { suggestions: Show[]; selectedShow?: { imdbId: string } }) {
-    const { suggestions, selectedShow } = props;
+function DropDown(props: { suggestions: Show[]; comboBoxProps: UseComboboxReturnValue<Show> }) {
+    const { suggestions } = props;
+    const { highlightedIndex, getItemProps } = props.comboBoxProps;
+
+    const rating = (show: Show) => (
+        <div className="shrink-0 flex items-center space-x-1 text-sm pl-2">
+            <dt className="text-sky-500">
+                <span className="sr-only">Star rating</span>
+                <svg width="16" height="20" fill="currentColor">
+                    <path d="M7.05 3.691c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.372 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.539 1.118l-2.8-2.034a1 1 0 00-1.176 0l-2.8 2.034c-.783.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.363-1.118L.98 9.483c-.784-.57-.381-1.81.587-1.81H5.03a1 1 0 00.95-.69L7.05 3.69z" />
+                </svg>
+            </dt>
+            <dd>{`${show.showRating.toFixed(1)} / 10.0`}</dd>
+        </div>
+    );
+
     return (
-        <div
-            role="listbox"
-            className="rounded-large p-1 border-small border-default-100 bg-background mt-2 absolute w-full"
-        >
+        <div className="rounded-large p-2 border-small border-default-100 bg-background mt-2">
             <ScrollShadow hideScrollBar className="max-h-[320px]">
                 <ul id="tv-search-dropdown">
-                    {suggestions.map((show) => (
+                    {suggestions.map((show, index) => (
                         <li
                             key={show.imdbId}
                             className={clsx(
                                 "rounded-medium",
                                 "text-default-500",
                                 "transition-opacity",
-                                { "bg-default-50 text-foreground": show.imdbId === selectedShow?.imdbId },
-
+                                { "bg-default-50 text-foreground": index === highlightedIndex },
                                 "hover:text-foreground",
                                 "hover:bg-default-200",
                                 "dark:hover:bg-default-50",
-
                                 "flex gap-2 items-center justify-between px-2 py-1.5 w-full h-full",
                                 "subpixel-antialiased cursor-pointer tap-highlight-transparent hover:transition-colors",
-
-                                // "data-[pressed=true]:opacity-70",
-                                // "data-[selectable=true]:focus:bg-default-100",
-                                // "data-[focus-visible=true]:ring-default-500",
                             )}
-                            // textValue={show.title}
+                            {...getItemProps({ item: show, index })}
                         >
-                            <Link
-                                tabIndex={-1}
-                                href={{ pathname: `/ratings/tt0417299` }}
-                                className="flex justify-between items-center"
-                            >
-                                <div className="flex gap-2 items-center">
-                                    <div className="flex flex-col">
-                                        <span className="text-small">{show.title}&nbsp;</span>
-                                        <span className="text-tiny text-default-400">{formatYears(show)}</span>
-                                    </div>
+                            <div className="flex justify-between w-full items-center">
+                                <div className="flex flex-col">
+                                    <span className="text-small break-words">{show.title}&nbsp;</span>
+                                    <span className="text-tiny text-default-400">{formatYears(show)}</span>
                                 </div>
-                            </Link>
+                                {rating(show)}
+                            </div>
                         </li>
                     ))}
                 </ul>
