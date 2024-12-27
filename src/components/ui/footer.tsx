b5f07004f7c62255aeaf35ba0a34f8eb78b96e4c
aamini <amini5454@gmail.com>
2024-12-27 01:25:26 -0500

    Update link

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -14,7 +14,7 @@ export function Footer() {
                     </a>
                     . The source code is available on{" "}
                     <a
-                        href="https://gitlab.com/imdbgraph"
+                        href="https://gitlab.com/users/aamini11/projects"
                         target="_blank"
                         rel="noreferrer"
                         className="font-medium underline underline-offset-4"
