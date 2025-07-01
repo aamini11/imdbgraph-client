798c991186dc586a5d0b42ffd61aa8842d6168c9
Aria <amini5454@gmail.com>
2025-07-01 01:45:11 -0400

    clean

--- src/components/search-bar.tsx
+++ src/components/search-bar.tsx
@@ -86,55 +86,53 @@ export function SearchBar() {
         )}
         {...getMenuProps()}
       >
-        {isOpen && inputValue && searchResults?.length === 0 && (
+        {inputValue && searchResults?.length === 0 && (
           <div className="text-foreground/60 px-2 py-1.5 text-center">
             No TV Shows Found.
           </div>
         )}
-        {isOpen &&
-          searchResults?.map((show, index) => (
-            <li
-              key={show.imdbId}
-              className={cn(
-                "text-foreground/60 hover:bg-foreground/5 flex cursor-pointer items-center justify-between gap-2 rounded-md px-2 py-1.5 text-sm outline-none select-none",
-                "data-[selected=true]:bg-accent data-[selected=true]:text-accent-foreground data-[disabled=true]:pointer-events-none data-[disabled=true]:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
-                {
-                  "bg-accent text-accent-foreground":
-                    highlightedIndex === index,
-                  "hover:bg-foreground/5": highlightedIndex !== index,
-                },
-              )}
-              {...getItemProps({
-                item: show,
-                index,
-              })}
-            >
-              <Link href={`/ratings`} className="flex w-full gap-2">
-                {/* Show Title + Years */}
-                <div className="flex flex-1 flex-col">
-                  <span className="break-words">{show.title}&nbsp;</span>
-                  <span className="text-foreground/40 text-xs">
-                    {formatYears(show)}
-                  </span>
-                </div>
-                {/* 1-10 Rating + Blue Star Icon */}
-                <div className="flex shrink-0 items-center space-x-1 text-sm">
-                  <dt>
-                    <span className="sr-only">Star rating</span>
-                    <svg
-                      className="text-sky-500"
-                      width="16"
-                      height="20"
-                      fill="currentColor"
-                    >
-                      <path d="M7.05 3.691c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.372 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.539 1.118l-2.8-2.034a1 1 0 00-1.176 0l-2.8 2.034c-.783.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.363-1.118L.98 9.483c-.784-.57-.381-1.81.587-1.81H5.03a1 1 0 00.95-.69L7.05 3.69z" />
-                    </svg>
-                  </dt>
-                  <dd>{`${show.rating.toFixed(1)} / 10.0`}</dd>
-                </div>
-              </Link>
-            </li>
-          ))}
+        {searchResults?.map((show, index) => (
+          <li
+            key={show.imdbId}
+            className={cn(
+              "text-foreground/60 hover:bg-foreground/5 flex cursor-pointer items-center justify-between gap-2 rounded-md px-2 py-1.5 text-sm outline-none select-none",
+              "data-[selected=true]:bg-accent data-[selected=true]:text-accent-foreground data-[disabled=true]:pointer-events-none data-[disabled=true]:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
+              {
+                "bg-accent text-accent-foreground": highlightedIndex === index,
+                "hover:bg-foreground/5": highlightedIndex !== index,
+              },
+            )}
+            {...getItemProps({
+              item: show,
+              index,
+            })}
+          >
+            <Link href={`/ratings`} className="flex w-full gap-2">
+              {/* Show Title + Years */}
+              <div className="flex flex-1 flex-col">
+                <span className="break-words">{show.title}&nbsp;</span>
+                <span className="text-foreground/40 text-xs">
+                  {formatYears(show)}
+                </span>
+              </div>
+              {/* 1-10 Rating + Blue Star Icon */}
+              <div className="flex shrink-0 items-center space-x-1 text-sm">
+                <dt>
+                  <span className="sr-only">Star rating</span>
+                  <svg
+                    className="text-sky-500"
+                    width="16"
+                    height="20"
+                    fill="currentColor"
+                  >
+                    <path d="M7.05 3.691c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.372 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.539 1.118l-2.8-2.034a1 1 0 00-1.176 0l-2.8 2.034c-.783.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.363-1.118L.98 9.483c-.784-.57-.381-1.81.587-1.81H5.03a1 1 0 00.95-.69L7.05 3.69z" />
+                  </svg>
+                </dt>
+                <dd>{`${show.rating.toFixed(1)} / 10.0`}</dd>
+              </div>
+            </Link>
+          </li>
+        ))}
       </ul>
     </search>
   );
